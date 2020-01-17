64e7ada56662f6b7f5d55376de035862dfc884c6
Peeke <pkuepers@kaliber.net>
2020-01-17 11:53:34 +0100

    Added Expand component to the example App

--- example/src/domain/App.js
+++ example/src/domain/App.js
@@ -1,18 +1,30 @@
 import { useElementSize }  from '@kaliber/use-element-size'
+import { Expand } from '/domain/Expand'
 import styles from './App.css'
 
 export default function App() {
   const elementRef = React.useRef(null)
   const { width, height } = useElementSize(elementRef)
+  const [expanded, setExpanded] = React.useState(false)
 
   return (
     <div className={styles.app}>
-      <div className={styles.box} ref={elementRef}>
+      <div ref={elementRef} className={styles.box}>
         <div className={styles.legend}>
           Resize browser
           <br />
           {Math.round(width)}px x {Math.round(height)}px
         </div>
+
+        <Expand {...{ expanded }}>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores magnam officiis nisi reiciendis architecto voluptate quam nobis cumque, modi quae aliquid possimus excepturi eveniet. Hic quos laudantium rerum magnam at!</p>
+          <p>Dolores magnam officiis nisi reiciendis architecto voluptate quam nobis cumque, modi quae aliquid possimus excepturi eveniet. Hic quos laudantium rerum magnam at!</p>
+          <p>Hic quos laudantium rerum magnam at! Dolores magnam officiis nisi reiciendis architecto voluptate quam nobis cumque, modi quae aliquid possimus excepturi eveniet. Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+        </Expand>
+
+        <button className={styles.button} onClick={() => setExpanded(!expanded)}>
+          {expanded ? 'Collapse' : 'Expand'}
+        </button>
       </div>
     </div>
   )
---
+++ example/src/domain/Expand.js
@@ -0,0 +1,15 @@
+import { useElementSize } from '@kaliber/use-element-size'
+import styles from './Expand.css'
+
+export function Expand({ children, expanded }) {
+  const innerRef = React.useRef(null)
+  const { height } = useElementSize(innerRef)
+
+  return (
+    <div className={styles.component} style={{ height: (expanded ? height : 0) + 'px' }}>
+      <div ref={innerRef} className={styles.inner}>
+        {children}
+      </div>
+    </div>
+  )
+}

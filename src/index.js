2875d210f47ad5883616418f3cb7a9901a4edcd3
Peeke <pkuepers@kaliber.net>
2020-03-20 16:21:15 +0100

    Re-use default size

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import { useObservedRef } from '@kaliber/use-observed-ref'
 
+const defaultSize = { width: 0, height: 0 }
+
 // assumes the resize-observer-polyfill is loaded, this can be done through polyfill.io
 export function useElementSize() {
-  const [size, setSize] = React.useState({ width: 0, height: 0 })
+  const [size, setSize] = React.useState(defaultSize)
   const createObserver = React.useCallback(
     () => {
       // @ts-ignore
@@ -13,7 +15,7 @@ export function useElementSize() {
     []
   );
 
-  const reset = React.useCallback(() => { setSize({ width: 0, height: 0 }) }, [])
+  const reset = React.useCallback(() => { setSize(defaultSize) }, [])
   const ref = useObservedRef({ createObserver, reset, disabled: false })
 
   return { size, ref }
